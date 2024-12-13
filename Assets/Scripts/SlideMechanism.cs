3ffd7369c87302d85871dfd4cdc3d5b0d5cf8648
Gordon Rose <gar018@bucknell.edu>
2024-12-13 18:50:11 -0500

    user interactable slide

--- Assets/Scripts/SlideMechanism.cs
+++ Assets/Scripts/SlideMechanism.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class SlideMechanism : MonoBehaviour
+public class SlideMechanism : XRSimpleInteractable
 {
     public float forcePower;
-
+    public float slidePullbackDistance;
     private Rigidbody rb;
+    private ConfigurableJoint cj;
 
-    private void Awake()
+    override protected void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        cj = GetComponent<ConfigurableJoint>();
+
+        base.Awake();
     }
     public void Fire()
     {
             rb.AddRelativeForce(Vector3.right*forcePower);
         
+    }
+
+    public void PullbackSlide() 
+    {
+        Debug.Log("AIMASSIST: I ACTUALLY PULL BACK THE SLIDE HAHAHAHAHAHAHAH");
+        cj.targetPosition = new Vector3(slidePullbackDistance,0,0);
+    }
+
+    public void ReleaseSlide()
+    {
+        Debug.Log("AIMASSIST: I ACTUALLY RELEASE THE SLIDE HAHAHAHAHAHAHAH");
+        cj.targetPosition = Vector3.zero;
     }
 }
 
